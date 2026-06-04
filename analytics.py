3ae333c3714821e51071c56d931a94e86c0ce8b3
ruslanakolisnyk <savovna@icloud.com>
2026-06-04 20:17:37 +0300

    changes to analytics.py, comments added

--- analytics.py
+++ analytics.py
@@ -1,7 +1,7 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
+
 df = pd.DataFrame({'Name': ['Olga', 'Maks', 'Slava'], 'Aga': [22, 34, 45]})
 df.head()
 print(df.head())
